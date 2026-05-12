c1dad8be916a745d0cf30381b25fa53f82ed5b8b
Silverhulk <hulksilver1@gmail.com>
2026-05-12 11:05:59 +0000

    Update Config.js

--- database/models/Config.js
+++ database/models/Config.js
@@ -4,7 +4,7 @@ const configSchema = new mongoose.Schema({
   pointSystem: {
     type: Map,
     of: Number,
-    default: () => new Map([['1',12],['2',9],['3',7],['4',5],['5',4],['6',3],['7',2],['8',1]])
+    default: () => new Map([['1',10],['2',6],['3',5],['4',4],['5',3],['6',2],['7',1],['8',1]])
   },
   killBonus: { type: Number, default: 1 },
   motd: { type: String, default: '' },
