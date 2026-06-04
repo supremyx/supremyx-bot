7e2ad0e2f8c906a0eeceb543ed4fcfbd4c515d49
Supremyx <supremyxci@gmail.com>
2026-06-04 16:57:12 +0000

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -369,7 +369,7 @@ function buildEmbed(page) {
   const p = PAGES[page];
   const embed = new EmbedBuilder()
     .setTitle(p.title)
-    .setColor(0x5865F2)
+    .setColor(0x87CEEB)
     .setDescription('*(staff)* = réservé aux administrateurs. Navigue avec les boutons ◀ ▶')
     .setFooter({ text: `SUPREMYX • Page ${page + 1} sur ${PAGES.length}` })
     .setTimestamp();
