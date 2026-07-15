59e591e8a5195ccffbe7a9db6904d00b07d0da0c
Supremyx <supremyxci@gmail.com>
2026-07-15 14:35:35 +0000

    Change welcome card text color from blue to green

--- utils/welcomeCard.js
+++ utils/welcomeCard.js
@@ -174,7 +174,7 @@ async function generateWelcomeCard({ member, title, subtitle, color, accentColor
   // Pseudo Discord de l'utilisateur (mis en avant, doré, sans "@")
   const username = member.user ? member.user.username : member.username;
   ctx.font = 'bold 30px sans-serif';
-  ctx.fillStyle = '#00BFFF';
+  ctx.fillStyle = '#1ABC9C';
   ctx.fillText(username, 48, 170);
 
   // Bannière du sous-titre (translucide, liseré doré)
